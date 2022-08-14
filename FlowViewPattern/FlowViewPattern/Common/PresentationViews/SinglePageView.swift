c437d9753f21dc0484ee3ed86e7c79d8b325b186
Thomas Asheim Smedmann <thomsmed@hotmail.com>
2022-08-14 14:10:59 +0200

    Hmm...

--- FlowViewPattern/FlowViewPattern/Common/PresentationViews/SinglePageView.swift
+++ FlowViewPattern/FlowViewPattern/Common/PresentationViews/SinglePageView.swift
@@ -7,14 +7,23 @@
 
 import SwiftUI
 
-struct SinglePageView: View {
+struct SinglePageView<Content: View>: View {
+
+    var content: () -> Content
+
+    init(selection: Binding<any Hashable>? = nil, @ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+
     var body: some View {
-        Text("")
+        TabView(content: content)
     }
 }
 
 struct SinglePageView_Previews: PreviewProvider {
     static var previews: some View {
-        SinglePageView()
+        SinglePageView() {
+
+        }
     }
 }
