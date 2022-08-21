243308cbfd587d77dea2f75b7eafc4a99dd8a99e
Thomas Asheim Smedmann <thomsmed@hotmail.com>
2022-08-21 18:13:02 +0200

    Wrap inside UINavigationController

--- DynamicTableHeaderView/DynamicTableHeaderView/SceneDelegate.swift
+++ DynamicTableHeaderView/DynamicTableHeaderView/SceneDelegate.swift
@@ -17,8 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
+        let navigationController = UINavigationController(
+            rootViewController: TableViewController(style: .insetGrouped)
+        )
+        navigationController.setNavigationBarHidden(true, animated: false)
+
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = TableViewController(style: .insetGrouped)
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
 
         self.window = window
