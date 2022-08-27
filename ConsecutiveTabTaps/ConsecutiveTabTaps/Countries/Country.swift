3d849084e6265b93628ae7b760e7e5e30eae3675
Thomas Asheim Smedmann <thomsmed@hotmail.com>
2022-08-27 20:14:11 +0200

    Add ConsecutiveTabTaps

---
+++ ConsecutiveTabTaps/ConsecutiveTabTaps/Countries/Country.swift
@@ -0,0 +1,13 @@
+//
+//  Country.swift
+//  ConsecutiveTabTaps
+//
+//  Created by Thomas Asheim Smedmann on 27/08/2022.
+//
+
+import Foundation
+
+struct Country: Identifiable, Hashable {
+    var id: String
+    let emojiFlag: String?
+}
