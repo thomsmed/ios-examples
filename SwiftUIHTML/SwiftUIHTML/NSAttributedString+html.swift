f7cf53651d157282019fb81a33d281df21024062
Thomas Asheim Smedmann <thomsmed@hotmail.com>
2023-07-12 20:35:59 +0200

    Add NSAttributedString+html

---
+++ SwiftUIHTML/SwiftUIHTML/NSAttributedString+html.swift
@@ -0,0 +1,61 @@
+//
+//  NSAttributedString+html.swift
+//  SwiftUIHTML
+//
+//  Created by Thomas Asheim Smedmann on 12/07/2023.
+//
+
+import UIKit
+
+/// Convenience extension to use with ``AttributedText`` if you don't want to make a dedicated SwiftUI View for HTML.
+extension NSAttributedString {
+    static func html(withBody body: String) -> NSAttributedString {
+        // Match the HTML `lang` attribute to current localisation used by the app (aka Bundle.main).
+        let bundle = Bundle.main
+        let lang = bundle.preferredLocalizations.first
+            ?? bundle.developmentLocalization
+            ?? "en"
+
+        return (try? NSAttributedString(
+            data: """
+            <!doctype html>
+            <html lang="\(lang)">
+            <head>
+                <meta charset="utf-8">
+                <style type="text/css">
+                    /*
+                      Custom CSS styling of HTML formatted text.
+                      Note, only a limited number of CSS features are supported by NSAttributedString/UITextView.
+                    */
+
+                    body {
+                        font: -apple-system-body;
+                        color: \(UIColor.secondaryLabel.hex);
+                    }
+
+                    h1, h2, h3, h4, h5, h6 {
+                        color: \(UIColor.label.hex);
+                    }
+
+                    a {
+                        color: \(UIColor.systemGreen.hex);
+                    }
+
+                    li:last-child {
+                        margin-bottom: 1em;
+                    }
+                </style>
+            </head>
+            <body>
+                \(body)
+            </body>
+            </html>
+            """.data(using: .utf8)!,
+            options: [
+                .documentType: NSAttributedString.DocumentType.html,
+                .characterEncoding: NSUTF8StringEncoding,
+            ],
+            documentAttributes: nil
+        )) ?? NSAttributedString(string: body)
+    }
+}
