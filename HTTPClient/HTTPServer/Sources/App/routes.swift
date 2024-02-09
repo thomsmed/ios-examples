e29a72f594bcbeeb968d711ef475d1f10345b27b
Thomas Asheim Smedmann <thomsmed@hotmail.com>
2024-02-10 00:49:06 +0100

    Basic CRUD for Items

--- HTTPClient/HTTPServer/Sources/App/routes.swift
+++ HTTPClient/HTTPServer/Sources/App/routes.swift
@@ -1,11 +1,76 @@
 import Vapor
 
+struct Item: Content {
+    let id: UUID
+    let text: String
+}
+
+struct ItemRequestDTO: Content {
+    let text: String
+}
+
+struct ItemResponseDTO: Content {
+    let id: UUID
+    let text: String
+}
+
+var items: [Item] = []
+
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    app.get("items") { req async throws in
+        items
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get("items", ":id") { req async throws in
+        guard let id = req.parameters.get("id", as: UUID.self) else {
+            fatalError("Expected id to be present")
+        }
+
+        guard let item = items.first(where: { $0.id == id }) else {
+            throw Abort(.notFound)
+        }
+
+        return ItemResponseDTO(id: item.id, text: item.text)
+    }
+
+    app.put("items", ":id") { req async throws in
+        guard let id = req.parameters.get("id", as: UUID.self) else {
+            fatalError("Expected id to be present")
+        }
+
+        let updatedItemDTO = try req.content.decode(ItemRequestDTO.self)
+
+        guard let index = items.firstIndex(where: { $0.id == id }) else {
+            throw Abort(.notFound)
+        }
+
+        let updatedItem = Item(id: id, text: updatedItemDTO.text)
+        items.replaceSubrange(index..<index, with: [updatedItem])
+
+        return ItemResponseDTO(id: updatedItem.id, text: updatedItem.text)
+    }
+
+    app.post("items") { req async throws in
+        let newItemDTO = try req.content.decode(ItemRequestDTO.self)
+
+        let newItem = Item(id: UUID(), text: newItemDTO.text)
+
+        items.append(newItem)
+
+        return ItemResponseDTO(id: newItem.id, text: newItem.text)
+    }
+
+    app.delete("items", ":id") { req async throws in
+        guard let id = req.parameters.get("id", as: UUID.self) else {
+            fatalError("Expected id to be present")
+        }
+
+        guard let index = items.firstIndex(where: { $0.id == id }) else {
+            throw Abort(.notFound)
+        }
+
+        let deletedItem = items.remove(at: index)
+
+        return ItemResponseDTO(id: deletedItem.id, text: deletedItem.text)
     }
 }
