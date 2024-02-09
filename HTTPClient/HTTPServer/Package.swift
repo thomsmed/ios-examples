e19372361351d13f9fab5d28e214a16fd4962f34
Thomas Asheim Smedmann <thomsmed@hotmail.com>
2024-02-09 19:36:45 +0100

    Add HTTPServer

---
+++ HTTPClient/HTTPServer/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "HTTPServer",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+            ]
+        ),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+
+            // Workaround for https://github.com/apple/swift-package-manager/issues/6940
+            .product(name: "Vapor", package: "vapor"),
+        ])
+    ]
+)
---
+++ HTTPClient/HTTPServer/Tests/AppTests/AppTests.swift
@@ -0,0 +1,15 @@
+@testable import App
+import XCTVapor
+
+final class AppTests: XCTestCase {
+    func testHelloWorld() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "hello", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "Hello, world!")
+        })
+    }
+}
