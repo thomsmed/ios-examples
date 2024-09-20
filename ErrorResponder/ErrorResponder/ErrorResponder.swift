a822e18200cc8f664083b18c62c15f4f86da206a
Thomas Asheim Smedmann <thomsmed@hotmail.com>
2024-09-20 22:59:25 +0200

    Add SwiftUI Environment

--- ErrorResponder/ErrorResponder/ErrorResponder.swift
+++ ErrorResponder/ErrorResponder/ErrorResponder.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 public enum ErrorEvaluation: Sendable {
     /// Necessary actions has been taken in response to the ``Error``.
@@ -25,8 +26,38 @@ public enum ErrorEvaluation: Sendable {
     case abort
 }
 
-public protocol ErrorResponder: AnyObject {
+@MainActor public protocol ErrorResponder: AnyObject {
     var parent: (any ErrorResponder)? { get set }
 
+    @discardableResult
     func respond(to error: any Error) async -> ErrorEvaluation
+}
+
+@MainActor public struct RespondToErrorAction {
+    let respondToError: (any Error) async -> ErrorEvaluation
+
+    @discardableResult
+    func callAsFunction(_ error: any Error) async -> ErrorEvaluation {
+        return await respondToError(error)
+    }
+}
+
+public struct RespondToErrorActionEnvironmentKey: EnvironmentKey {
+    public static let defaultValue: RespondToErrorAction = RespondToErrorAction { _ in
+        assertionFailure("Unhandled error")
+        return .proceed
+    }
+}
+
+public extension EnvironmentValues {
+    var respondToError: RespondToErrorAction {
+        get { self[RespondToErrorActionEnvironmentKey.self] }
+        set { self[RespondToErrorActionEnvironmentKey.self] = newValue }
+    }
+}
+
+public extension View {
+    func respondToError(_ respondToError: @escaping (any Error) async -> ErrorEvaluation) -> some View {
+        environment(\.respondToError, RespondToErrorAction(respondToError: respondToError))
+    }
 }
