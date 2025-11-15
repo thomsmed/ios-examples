b7c73a99722aff67709ab1df12cd7869312a96f3
Thomas Asheim Smedmann <thomsmed@hotmail.com>
2025-11-15 22:19:44 +0100

    Add HTTPSAndLocalhost

---
+++ Localhost/Localhost/LocalhostApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalhostApp.swift
+//  Localhost
+//
+//  Created by Thomas Asheim Smedmann on 14/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct LocalhostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
